620148f3b2eb172836f2ba0f3254a2d4be2f215d
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 19:17:52 +0000

    automatically backed up by learn

--- looping.rb
+++ looping.rb
@@ -1,12 +1,16 @@
-counter = 0
+# counter = 0
+# 
+# loop do
+#   counter += 1
+#   puts "I have found the Time Machine!"
+# 
+#   if counter >=10
+#     break
+#   end
+# end
 
 loop do
-  counter += 1
   puts "I have found the Time Machine!"
-
-  if counter >=10
-    break
-  end
 end
 
 #call your method here
